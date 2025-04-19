c9491c6e372bf990134eb09b485fa781d3f04237
que mienten <xyek5555@gmail.com>
2025-04-19 17:13:10 +0300

    Update script.m

--- matlab/lab-6/script.m
+++ matlab/lab-6/script.m
@@ -1,5 +1,5 @@
 % opción 22
-%% número 1 (gráficos)
+%% número 1 (графики и решение fsolve)
 clc, clearvars, close all;
 % Система уравнений:
 % 2x + y^3 = 3
@@ -27,37 +27,69 @@ plot(xr(1), xr(2), 'k*')
 legend('Y1', 'Y2', 'desicion'); 
 
 
-%% número 2 (gráficos)
-
-lambda = [-1/3; -1/2];
+%% número 3 (решение МПИ)
 
 clc, clearvars, close all;
-
-% Правильные уравнения для графиков
-x = linspace(-2, 2, 1000);  % Изменил диапазон для лучшей визуализации
-y1 = (3 - 2*x).^(1/3);      % y^3 = 3 - 2x
-y2 = 3 - 2*x.^3;            % y = 3 - 2x^3
-
-plot(x, y1, 'b')
-hold on;
-plot(x, y2, 'r')
-title('Графики функций y = y(x)'), xlabel('x'), ylabel('y'), grid on;
-ylim([-5 5]);
-xlim([-2 2]);
-
-% Функция для fsolve (должна соответствовать графикам)
+ 
+lambda = [-1/3; -1/2];
 function f = func(x)
-    f = [2*x(1) + x(2)^3 - 3;   % 2x + y^3 = 3 (как в графике y1)
-         2*x(1)^3 + x(2) - 3];   % 2x^3 + y = 3 (как в графике y2)
+    f(1) = x(1) - 1/3*(2*x(1) + x(2)^3 - 3);   % 2x + y^3 = 3 (как в графике y1)
+    f(2) = x(2) - 1/2 * (2*x(1)^3 + x(2) - 3);   % 2x^3 + y = 3 (как в графике y2)
 end
 
-% Находим решение
-initial_guess = [1; 1];
-xr = fsolve(@func, initial_guess);
+x = [1 1 1];
+maxIter = 2;
+for i = 1:maxIter
+    R = subs(subs(jacobian(f, x),x1,x(1)), x2, x(2));
+end
+      
+%%
+clc, clearvars, close all;
+ 
+phi = @(x) [
+    (3 - x(2)^3)/2;
+    (3 - 2*x(1)^3)/1
+];
+x0 = [0.5; 0.5];
+e = 1e-3;
+x = x0;
+maxIter = 100; 
+iter = 0;
+R = 1; 
+   
+f = [x1^2 + x2;
+     x2*exp(x3);
+     x1 + x3^2];
 
-% Отображаем решение
-plot(xr(1), xr(2), 'ko')
-legend('y1 = (3-2x)^{1/3}', 'y2 = 3-2x^3', 'Решение');
+while R == 1 && iter < maxIter
+    x_old = x;
+    x = phi(x_old); 
+
+    delta = x - x_old;
+    if max(delta) < e;
+        fprintf('Достигнута точнсть на %d итерации, x:', iter);
+        disp(x);
+        break
+    end
+
+    J = jacobian(phi, x); 
+    J_numeric = subs(J, {x1, x2}, {x(1), x(2), x(3)});
+    J_numeric = double(J_numeric);
+    if sum(J_numeric(1,:)) > 1 || sum(J_numeric(2,:)) > 1 || sum(J_numeric(3,:)) > 1
+        fprintf('Условие сходиммости не выполняется на %d итерации, x:', iter);
+        disp(x);
+        break
+    end
+    iter = iter + 1;
+end
+
+    if R == 1
+        warning('Достигнуто максимальное число итераций без достижения точности!');
+    end
+
+fprintf('\nРешение найдено за %d итераций:\n', iter);
+fprintf('x1 = %.8f\nx2 = %.8f\n', x(1), x(2));
+
 
 
 
