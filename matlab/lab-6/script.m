dd7d0a7628b4e544a70746639d1edf2cec0e3e7e
que mienten <xyek5555@gmail.com>
2025-04-22 12:02:30 +0300

    Update script.m

--- matlab/lab-6/script.m
+++ matlab/lab-6/script.m
@@ -24,23 +24,22 @@ clc, clearvars, close all;
 
 
 e = 1e-3;
-x0 = [-0.3 -0.3];
+x0 = [0.1 0.1];
 x = x0;
 R = 1;
 
-phi = @(x) [(3 - x(2)^3)/2, 3 - 2*x(1)^3];
+phi = @(x) [((3 - x(2))/2)^(1/3), (3 - 2*x(1))^(1/3)];
 
-syms x1 x2
-phi_sym = [(3 - x2^3)/2;
-           3 - 2*x1^3];
+syms x1 x2;
+phi_sym = [((3 - x2)/2)^(1/3), (3 - 2*x1)^(1/3)];
 J = jacobian(phi_sym, [x1 x2]);
 
-maxIter = 20;
+maxIter = 10;
 i = 0;
 while(i ~= maxIter)
     xOld = x;
     x = phi(x);
-     
+
     Jr = double(subs(J, [x1, x2], x));
     R = sum(abs(Jr(1,:)))<1 || sum(abs(Jr(2,:)))<1;
     if R == 0
@@ -51,7 +50,7 @@ while(i ~= maxIter)
 
     if max(abs(x - xOld))<e
         fprintf('Достигнута точность эпсилон на %d итерации, x:', i+1);
-        % x - xOld;
+        % x - xOld
         disp(x);
         break;
     end
@@ -62,3 +61,72 @@ if (i == maxIter)
     fprintf('После iterMax = %d точность эпсилон не была достигнута, x:', i+1);
     disp(x);
 end
+
+%% номер 3 (решение методом ПИ зейдаля)
+clc, clearvars, close all;
+
+
+e = 1e-3;
+x0 = [0.1 0.1];
+x = x0;
+R = 1;
+
+phi_1 = @(x) ((3 - x)/2)^(1/3);
+phi_2 = @(x) (3 - 2*x)^(1/3);
+
+syms x1 x2;
+phi_sym = [((3 - x2)/2)^(1/3), (3 - 2*x1)^(1/3)];
+J = jacobian(phi_sym, [x1 x2]);
+
+maxIter = 10;
+i = 0;
+while(i ~= maxIter)
+    xOld = x;
+    x(1) = phi_1(x(2));
+    x(2) = phi_2(x(1));
+
+    Jr = double(subs(J, [x1, x2], x));
+    R = sum(abs(Jr(1,:)))<1 || sum(abs(Jr(2,:)))<1;
+    if R == 0
+        fprintf('Условия сх не вып-ся на %d итерации, x:', i+1);
+        disp(x);
+        break;
+    end
+
+    if max(abs(x - xOld))<e
+        fprintf('Достигнута точность эпсилон на %d итерации, x:', i+1);
+        % x - xOld
+        disp(x);
+        break;
+    end
+
+    i = i + 1;
+end
+if (i == maxIter)
+    fprintf('После iterMax = %d точность эпсилон не была достигнута, x:', i+1);
+    disp(x);
+end
+
+%% номер 4 (решение методом ньютона)
+clc, clearvars, close all;
+
+syms x1 x2;
+f_sym = [((3 - x2)/2)^(1/3), (3 - 2*x1)^(1/3)];
+f = @(x) [(2*x(1)-x(2)^3); (2*x(1)^3 + x(2))];
+x0 = [1; 1];
+e = 1e-3;
+
+
+A = jacobian(f_sym, [x1 x2]);
+B = f;
+H = x0;
+
+
+maxIter = 0;
+for i = 1:maxIter
+    H = double(subs(A, [x1, x2], H))
+    break;
+end
+
+
+
