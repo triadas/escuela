b8cf1fe9a82ce3752cd7c2a53918ab51c5ed8813
que mienten <xyek5555@gmail.com>
2025-06-05 01:34:23 +0300

    LOL

---
+++ Desarrollo web/0.js
@@ -0,0 +1,7 @@
+var num1 = window.prompt("Введите первое число: ");
+var num2 = window.prompt("Введите второе число: ");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+document.write(num1 + num2);
